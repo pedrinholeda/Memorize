b4afe647cd7e9c15d95245552b7c0c03d86e418c
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-04-10 03:00:03 -0300

    finished lecture one about introduction to SwiftUI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,14 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-            Text("🐼")
-        })
-        .padding()
-        .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
+         HStack{
+            ForEach(0..<4)  { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+
+struct CardView: View{
+    var isFaceUp: Bool
+    
+    var body: some View{
+        ZStack {
+            if isFaceUp{
+               RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+               RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+               Text("🐼")
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+       }
     }
 }
 
